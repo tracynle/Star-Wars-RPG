a34ed363356f7b7b695450e280b25a16b5ed54c1
Tracy Le <tnle9@asu.edu>
2018-12-13 18:05:51 -0800

    HTML and CSS layout ggrid, images and music added.

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+
+//     var starWars = $(".starWars");
+
+//     // Gets Link for Theme Song
+//     var audioElement = document.createElement("audio");
+//     audioElement.setAttribute("src", "assets/star-wars-theme-song.mp3");
+//     // Theme Button
+//     $(".theme-button").on("click", function() {
+//         audioElement.play();
+//       });
+//       $(".pause-button").on("click", function() {
+//         audioElement.pause();
+//       });
+
+
+// });
