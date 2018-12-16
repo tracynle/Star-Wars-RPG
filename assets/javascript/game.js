8ad177f9b6177066e7ac7545b568164bd42876ac
Tracy Le <tnle9@asu.edu>
2018-12-15 21:31:05 -0800

    Edited JavaScript and changed HTML sections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,10 @@
-// $(document).ready(function() {
 
-//     var starWars = $(".starWars");
+$(".character-button").on("click", function() {
+    var character = $(this).attr('data-character');
+    console.log("User clicked: ", character);
+});
 
-//     // Gets Link for Theme Song
-//     var audioElement = document.createElement("audio");
-//     audioElement.setAttribute("src", "assets/star-wars-theme-song.mp3");
-//     // Theme Button
-//     $(".theme-button").on("click", function() {
-//         audioElement.play();
-//       });
-//       $(".pause-button").on("click", function() {
-//         audioElement.pause();
-//       });
-
-
-// });
+$(".attack").on("click", function() {
+    var audio = new Audio('assets/light-saber-battle.mp3');
+    audio.play();
+});
